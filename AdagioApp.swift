f0160041a83d5952b6a5762afc278e14a795cf43
Darrenk328 <dmkapturski@gmail.com>
2026-08-24 23:08:57 -0400

    Initial commit

---
+++ AdagioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdagioApp.swift
+//  Adagio
+//
+//  Created by Ryan Soudkhah on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct AdagioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
